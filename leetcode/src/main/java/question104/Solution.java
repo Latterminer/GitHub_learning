12d960f710a08f701cb7cb6d053f5c7bf738a074
wy <autumnimagery@outlook.com>
2019-05-16 00:19:22 +0800

    添加question942

---
+++ leetcode/src/main/java/question104/Solution.java
@@ -0,0 +1,29 @@
+package question104;
+
+/**
+ * @author wangyong
+ * @date 2019/5/15
+ * @description 求二叉树的深度
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left, right);
+    }
+
+
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
