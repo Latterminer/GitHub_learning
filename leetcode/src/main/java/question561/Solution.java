bc9571f843acc559c38b896af32baf083845d01a
wy <autumnimagery@outlook.com>
2019-05-25 23:43:05 +0800

    add 561

---
+++ leetcode/src/main/java/question561/Solution.java
@@ -0,0 +1,27 @@
+package question561;
+
+import java.util.Arrays;
+
+/**
+ * @author wangyong
+ * @date 2019/5/25
+ * @description
+ * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,4,3,2]
+ *
+ * 输出: 4
+ * 解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+ */
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i=i+2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
