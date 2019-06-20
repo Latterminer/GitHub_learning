9a79a8fc68c09f24ac61ec799fb5fc7c6fd1c4c6
wy <autumnimagery@outlook.com>
2019-06-21 00:20:20 +0800

    add 538

---
+++ leetcode/src/main/java/com/wy/leetcode/question538_/Solution.java
@@ -0,0 +1,57 @@
+package com.wy.leetcode.question538_;
+
+/**
+ * @author wangyong
+ * @date 2019/6/20
+ * @description
+ * 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+ *
+ * 例如：
+ *
+ * 输入: 二叉搜索树:
+ *               5
+ *             /   \
+ *            2     13
+ *
+ * 输出: 转换为累加树:
+ *              18
+ *             /   \
+ *           20     13
+ *
+ */
+public class Solution {
+    private int sum = 0;
+
+    /**
+     * 逆中序遍历， 右 -> 中 -> 左
+     * @param root
+     * @return
+     */
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        sumRightTree(root);
+        return root;
+    }
+
+    private void sumRightTree(TreeNode root) {
+        if (root == null) {
+            return ;
+        }
+        sumRightTree(root.right);
+        root.val += sum;
+        sum = root.val;
+        sumRightTree(root.left);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
